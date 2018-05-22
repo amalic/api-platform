66749285d836b30facc4fbdbca78527df24b9f86
Alexander Malic <alexander.malic@gmail.com>
2018-05-22 02:41:45 +0200

    embedded drill into api-platform

---
+++ drill/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Start Apache Drill in embedded mode
+# https://drill.apache.org/docs/starting-drill-on-linux-and-mac-os-x/
+#
+echo "Starting Apache Drill in tmux session called drill-embedded"
+tmux new-session -d -s "drill-embedded" /opt/drill/apache-drill-1.13.0/bin/drill-embedded
+echo "running"
+sleep infinity
